708e03278b09b21c6dbae5051106f3e25980dd90
Camilo Polymeris <camilo@polymeris.dev>
2021-01-23 09:45:17 -0300

    Add BTG quotes

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Scripts para diariamiente descargar indicadores, como las cuotas de AFP.
 Datos en la rama [data](https://github.com/polymeris/moneas/tree/data/out), actualizados diariamente.
 Sin embargo, los valores pueden cambiar, ya que la superintendencia de pensiones solo confirma el valor
 al mes siguiente.
+
+### Tests
+
+`./src/afp_quotes.clj test`
---
+++ src/btg_quotes.clj
@@ -0,0 +1,62 @@
+#!/usr/bin/env -S bb --classpath src
+
+(require '[cheshire.core :as json]
+         '[clojure.test :refer [deftest is run-tests]]
+         '[util])
+
+(def base-url "https://www.btgpactual.cl/")
+
+(def funds-symbol->path {"CFIBTGRCA" "fondo-de-inversion/renta-comercial/"
+                         "CFIBTGRRA" "fondo-de-inversion/renta-residencial/"
+                         "CFIREMERGE" "fondo-de-inversion/infraestructura/"
+                         "CFIBTGCYFA" "fondo-de-inversion/credito-y-facturas/"
+                         "CFIBTGFGIA" "fondo-de-inversion/financiamiento-con-garantias-inmobiliarias/"
+                         "CFIMHEA-E" "fondo-de-inversion/mhe-habitacional/"})
+
+(defn extract-quotes
+  [data]
+  (->> data :perf :performance :entry
+       (map (fn [q] {:date (-> q :key (subs 0 10))
+                     :value (-> q :value bigdec)}))))
+
+(deftest extract-quotes-test
+  (is (= [{:date "2020-01-01", :value 1.5M}
+          {:date "2020-01-02", :value 2M}]
+         (extract-quotes {:perf {:performance {:entry [{:key "2020-01-01T00:00:00-3", :value 1.5}
+                                                       {:key "2020-01-02T00:00:00-3", :value 2}]}}}))))
+
+(defn merge-quotes
+  [old new]
+  (->> (merge (into {} (map (juxt :date :value) old))
+              (into {} (map (juxt :date :value) new)))
+       (map (fn [[date value]]
+              {:date date
+               :value value}))
+       (sort-by :date)))
+
+(deftest merge-quotes-test
+  (is (= [{:date 1 :value 100}, {:date 2 :value 150}, {:date 3 :value 200}])
+      (merge-quotes [{:date 1 :value 100}, {:date 2 :value 120}]
+                    [{:date 2 :value 150}, {:date 3 :value 200}]))
+  (is (= [{:date 3 :value 200}, {:date 4 :value 500}])
+      (merge-quotes nil [{:date 3 :value 200}, {:date 4 :value 500}])))
+
+(defn -main
+  []
+  (doseq [[sym path] funds-symbol->path]
+    (let [filename (str "out/" sym ".json")
+          page (util/fetch-url! (str base-url path))
+          [_ json-data] (re-find #"var jsonFondos = '(.*)';" page)
+          new-quotes (extract-quotes (json/parse-string json-data true))
+          old-quotes (try (json/parse-string (slurp filename) true)
+                          (catch java.io.FileNotFoundException _ex
+                            (println "Warning. File not found:" filename)
+                            nil))
+          quotes (->> (merge-quotes old-quotes new-quotes)
+                      (map (fn [q] (assoc q :symbol sym))))]
+      (util/write-json! filename quotes))))
+
+(if (= "test" (first *command-line-args*))
+  (let [{:keys [:fail :error]} (run-tests)]
+    (System/exit (+ fail error)))
+  (-main))
